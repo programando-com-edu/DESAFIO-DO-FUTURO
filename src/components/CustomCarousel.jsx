8d00425ac5805ef9ffe1bdd9f5241e358f6be8c4
Eduardo <edu.rocha.aug@gmail.com>
2023-11-29 23:01:22 -0300

    carousel data

--- src/components/CustomCarousel.jsx
+++ src/components/CustomCarousel.jsx
@@ -1,11 +1,9 @@
-// src/App.js
 import React from 'react';
 import "react-responsive-carousel/lib/styles/carousel.min.css";
 import { Carousel } from 'react-responsive-carousel';
 import bannerDesafio from '../assets/bannerDesafio.jpeg';
 
-
-function CustomCarousel() {
+function CustomCarousel({ dataItems }) {
     const settings = {
         showArrows: false,
         centerMode: true,
@@ -13,19 +11,22 @@ function CustomCarousel() {
         itemPadding: '30px',
         showThumbs: false,
         showStatus: false,
-        showIndicators: false 
+        showIndicators: false,
+        
     };
-  return (
-    <Carousel {...settings} className='mt-2 ml-3'>
-        <div className='mx-2 h-full flex justify-center items-end flex-shrink-0 relative'>
-            <img src="https://s2-ge.glbimg.com/D0Dr5Esd8DVua644sFleZiFiZr4=/0x0:1920x1080/1080x608/smart/filters:max_age(3600)/http://s01.video.glbimg.com/deo/vi/87/92/12129287" alt="" className='rounded-lg absolute' />
-            <p className='z-10 text-white text-left font-semibold drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,0.8)]'>Desafio do futuro: Marcos Montenegro e Antero têm convidado especial como "cobaia"</p>
-        </div>
-        <div className='mx-2 h-full flex justify-center items-end flex-shrink-0 relative'>
-            <img src={bannerDesafio} alt="" className='rounded-lg' />
-        </div>
-    </Carousel>
-  );
+
+    return (
+        <Carousel {...settings} className='mt-2 ml-3'>
+            {dataItems && dataItems.map(item => (
+                <div key={item.id} className='mx-2 h-36 flex justify-center items-end flex-shrink-0 relative'>
+                    <img src={item.link} alt="" className='absolute rounded-lg' />
+                    <p className='z-10 text-sm text-white text-left font-semibold drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,0.8)]'>
+                        {item.text}
+                    </p>
+                </div>
+            ))}
+        </Carousel>
+    );
 }
 
 export default CustomCarousel;
